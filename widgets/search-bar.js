93ef0d43df27ec329969f8deeb7a8cbb1fc40c0e
Kristian Tuomela <123019726+tu0m@users.noreply.github.com>
2024-03-20 17:36:21 +0200

    Initial working search bar widget

---
+++ widgets/search-bar.js
@@ -0,0 +1,119 @@
+import sheet from './style.css?inline'
+import searchEngines from './searchengines.json'
+
+class SearchBar extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        // attributeChangedCallback() is triggered only by the attributes listed here
+        return ['searchengine']
+    }
+
+    connectedCallback() {
+        const shadowRoot = this.attachShadow({ mode: "open" })
+
+        // add css
+        const moduleSheet = new CSSStyleSheet()
+        moduleSheet.replaceSync(sheet)
+        shadowRoot.adoptedStyleSheets = [moduleSheet]
+
+        // add html
+        const layout = `
+        <label for="searchbar"></label>
+        <div class="container">
+            <input type="search" id="searchbar" name="q" />
+            <span class="tooltip" hidden>Tab</span>
+        </div>
+        <button>➤</button>
+        `
+        this.shadowRoot.innerHTML = layout
+
+        // dom references
+        this.label = this.shadowRoot.querySelector('label')
+        this.input = this.shadowRoot.querySelector('input')
+        this.button = this.shadowRoot.querySelector('button')
+
+        // event listeners
+        this.label.addEventListener('click', this.showSearchEngines)
+        this.input.addEventListener('keydown', this.keyPressHandler)
+        this.input.addEventListener('input', this.toggleTabIcon)
+        this.button.addEventListener('click', this.search)
+    }
+
+    disconnectedCallback() {
+        this.label.removeEventListener('click', this.showSearchEngines)
+        this.input.removeEventListener('keydown', this.keyPressHandler)
+        this.input.addEventListener('input', this.toggleTabIcon)
+        this.button.removeEventListener('click', this.search)
+    }
+
+    async attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) return
+
+        // this thing waits for this.label to become defined, because attributeChangedCallback() is called before connectedCallback()
+        while (this.label == undefined) {
+            await new Promise(resolve => requestAnimationFrame(resolve))
+        }
+
+        this.label.innerText = newValue
+        this.searchEngineName = newValue
+    }
+
+    showSearchEngines = (e) => {
+        // TODO: open popup that lists all available search engines
+        // update search-bar attribute on click?
+    }
+
+    changeSearchEngine(key) {
+        if (searchEngines[key].name.length) {
+            this.setAttribute('searchengine', searchEngines[key].name)
+        }
+    }
+
+    keyPressHandler = (e) => {
+        if (e.key == 'Tab') {
+            if (this.searchQuery.length != 1) return
+            if (searchEngines[this.searchQuery.toLowerCase()]?.name.length) {
+                e.preventDefault()
+                this.setAttribute('searchengine', searchEngines[this.searchQuery.toLowerCase()].name)
+                this.input.value = ""
+                this.toggleTabIcon()
+            }
+        }
+
+        if (e.key == 'Enter') {
+            if (this.searchQuery.length == 0) return
+            this.search()
+            this.input.value = ""
+            this.toggleTabIcon()
+        }
+    }
+
+    toggleTabIcon = (e) => {
+        if (this.searchQuery.length == 1 && searchEngines[this.searchQuery.toLowerCase()]?.name.length) {
+            this.shadowRoot.querySelector('span').removeAttribute('hidden')
+        } else {
+            this.shadowRoot.querySelector('span').setAttribute('hidden', true)
+        }
+    }
+
+
+    get searchQuery() {
+        return this.input.value
+    }
+
+    get searchEngineUrl() {
+        // what a monster... TODO: maybe make this more readable some day
+        return Object.values(searchEngines).filter(item => item.name == this.searchEngineName)[0].url
+    }
+
+    search = () => {
+        const url = this.searchEngineUrl + this.searchQuery
+        window.open(url, '_blank');
+    }
+}
+
+customElements.define("search-bar", SearchBar);
