82bfb576443089f56df078c69c51fef9d644df61
Kristian Tuomela <123019726+tu0m@users.noreply.github.com>
2024-03-23 20:00:02 +0200

    Add general widget-related functionality

--- widgets/index.js
+++ widgets/index.js
@@ -1,28 +1,24 @@
-import './search-bar.js'
+import './search-widget.js'
 import './clock-widget.js'
 import './date-widget.js'
 import './link-widget.js'
 
 import searchEngines from './searchengines.json'
 
-const searchEngineArray = () => {
-    let array = []
-    Object.values(searchEngines).forEach(value => {
-        if (value.name) array.push(value.name)
-    })
-    return array
-}
-
-export const Widgets = [
+const library = [
     {
         type: "Search",
-        tag: `<search-bar class="bar widget" searchengine></search-bar>`,
-        attributes: { 'searchengine': searchEngineArray() }, // TODO: how to get all availabe search engines here?
+        tag: `<search-widget class="bar widget" data-searchengine></search-widget>`,
+        attributes: {
+            'data-searchengine': _searchEngineNames()
+        },
     },
     {
         type: "Clock",
-        tag: `<clock-widget class="square widget" city></clock-widget>`,
-        attributes: { 'city': 'Location (currently not working)' },
+        tag: `<clock-widget class="square widget" data-city></clock-widget>`,
+        attributes: {
+            'data-city': 'Location (currently not working)'
+        },
     },
     {
         type: "Date",
@@ -31,10 +27,32 @@ export const Widgets = [
     },
     {
         type: "Link",
-        tag: `<link-widget class="square widget" tabindex="0" name url></link-widget>`,
+        tag: `<link-widget class="square widget" tabindex="0" data-name data-url></link-widget>`,
         attributes: {
-            'name': 'Website name',
-            'url': 'URL'
+            'data-name': 'Website name',
+            'data-url': 'URL'
         },
     },
 ]
+
+function _searchEngineNames() {
+    let array = []
+    Object.values(searchEngines).forEach(value => {
+        if (value.name) array.push(value.name)
+    })
+    return array
+}
+
+function types() {
+    return library.map(item => item.type)
+}
+
+function attributes(type) {
+    return library.find(item => item.type == type).attributes
+}
+
+function htmlTag(type) {
+    return library.find(item => item.type == type).tag
+}
+
+export { library, types, attributes, htmlTag };
