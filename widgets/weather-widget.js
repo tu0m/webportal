e98a099474de4f776e44b5d7d21903a4094e4ae1
Kristian Tuomela <123019726+tu0m@users.noreply.github.com>
2024-03-31 17:28:55 +0300

    All basic functionality done (needs testing)

--- widgets/weather-widget.js
+++ widgets/weather-widget.js
@@ -31,16 +31,19 @@ class WeatherWidget extends HTMLElement {
         shadowRoot.innerHTML = layout
 
         // event listeners
+        shadowRoot.host.addEventListener('keydown', this.keyPressHandler)
+        shadowRoot.host.addEventListener('click', this.keyPressHandler)
 
         // update weather
-        this.interval = setInterval(() => {
-            this.update()
-        }, 3600000); // 1 hour 
+        this.updateInterval = setInterval(this.update, 600000); // 10 minutes
     }
 
     disconnectedCallback() {
-        clearInterval(this.interval)
-        // delete localStorage "weather-widget"
+        clearInterval(this.updateInterval)
+        clearInterval(this.setTextInterval)
+        this.deleteAll()
+        this.shadowRoot.host.removeEventListener('keydown', this.keyPressHandler)
+        this.shadowRoot.host.removeEventListener('click', this.keyPressHandler)
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
@@ -54,45 +57,36 @@ class WeatherWidget extends HTMLElement {
     }
 
     async update() {
-
-
-
         let coords = null
         let weatherData = null
+        let nextHour = new Date()
+        nextHour.setHours(nextHour.getHours() + 1)
+        nextHour = nextHour.toISOString()
 
         // check localStorage
-        let saveData = storage.load()
+        const saveData = this.load
         if (saveData) [coords, weatherData] = [saveData['coords'], saveData['weatherData']]
 
-        // fetch coords and weatherData
+        // check if weather data is more than 1 day old
+        const tooOld = nextHour.slice(0, 10) == weatherData?.properties.meta.updated_at.slice(0, 10) ? false : true
+
+        // fetch coords and weatherData if needed
         if (!coords) coords = await this.fetchCoords(this.city, this.country)
-        if (coords && !weatherData) weatherData = await this.fetchWeatherData(coords)
+        if (coords && (!weatherData || tooOld)) weatherData = await this.fetchWeatherData(coords)
+        // if fetch fails just return
+        // TODO: add tests and test network issues there?
+        if (!coords || !weatherData) return
+        this.save(coords, weatherData)
 
-        // TODO: coords or weatherData can still be null or undefined because of network issues or incorrect data, handle it?
+        // TODO: if precipitation is >0, show the amount in the widget (but if there's multiple weather widgets, they will go out of sync due to having different amount of text items)
+        const currentHourWeatherData = weatherData.properties.timeseries.find(object => object.time.startsWith(nextHour.slice(0, 13)))
+        const symbolCode = currentHourWeatherData?.data.next_1_hours.summary.symbol_code
+        const currTemp = Math.round(currentHourWeatherData?.data.instant.details.air_temperature) + ' °C'
+        const currWind = Number(currentHourWeatherData?.data.instant.details.wind_speed) + ' m/s'
+        const currPrec = Number(currentHourWeatherData?.data.next_1_hours.details.precipitation_amount) + ' mm'
 
-        const currentTime = new Date().toISOString()
-        const currentHourWeatherData = weatherData.properties.timeseries.find(object => object.time.startsWith(currentTime.slice(0, 13)))
-
-        const currTemp = Math.round(currentHourWeatherData.data.instant.details.air_temperature) + '°C'
-        const symbolCode = currentHourWeatherData.data.next_1_hours.summary.symbol_code
-
-        console.log(currTemp)
-        console.log(symbolCode)
-        console.log(currentHourWeatherData)
-
-
-
-
-        // check if weatherData is still valid (if current hour is found)
-
-        // set city name, weather icon, temperature
-
-        // save data to localStorage
-
-        this.setCity(this.city)
         this.setIcon(symbolCode)
-        this.setTemp(currTemp)
-
+        this.setText(this.city, currTemp, currWind, currPrec)
     }
 
     async fetchCoords(city, country, timer = 1) {
@@ -119,7 +113,7 @@ class WeatherWidget extends HTMLElement {
                 throw new Error('Failed to find coordinates')
 
             }).catch(error => {
-                // There was an error
+                this.classList.add('error')
                 console.log(error)
                 console.log('Try deleting and re-adding weather widget and check that both city and country are entered correctly')
                 return null
@@ -147,10 +141,12 @@ class WeatherWidget extends HTMLElement {
 
             }).then(data => {
                 // This is the JSON from our response
+                const time = new Intl.DateTimeFormat(undefined, { timeStyle: "short" }).format(Date.now())
+                console.log('Fresh weather data fetched at ' + time)
                 return data
 
             }).catch(error => {
-                // There was an error
+                this.classList.add('error')
                 console.log(error)
                 console.log('Try deleting and re-adding weather widget and check that both city and country are entered correctly')
                 return null
@@ -158,31 +154,67 @@ class WeatherWidget extends HTMLElement {
     }
 
     save(coords, weatherData) {
-        storage.save('weather-widget', { coords: coords, weatherData: weatherData })
+        const uuid = this.getAttribute('uuid')
+        const data = { coords: coords, weatherData: weatherData }
+        storage.save(data, uuid)
     }
 
     get load() {
-        return storage.load('weather-widget')
+        const uuid = this.getAttribute('uuid')
+        return storage.load(uuid)
+    }
+
+    deleteAll() {
+        const uuid = this.getAttribute('uuid')
+        storage.remove(uuid)
     }
 
     getIcon(symbolCode) {
         return new URL(`/icons/weather-widget/${symbolCode}.svg`, import.meta.url).href
     }
 
-    setCity(city) {
-        // TODO: show city name on mouse hover/touch or make a slow carousel of city, temp, wind, etc
-        // this.shadowRoot.querySelector('.top').textContent = city
-    }
-
     setIcon(symbolCode) {
+        if (!symbolCode) return
         const icon = document.createElement('img')
         icon.src = this.getIcon(symbolCode)
         icon.alt = 'current weather'
+        icon.draggable = false
         this.shadowRoot.querySelector('.mid').replaceChildren(icon)
     }
 
-    setTemp(temp) {
-        this.shadowRoot.querySelector('.btm').textContent = temp
+    setText(...items) {
+        if (!items) return
+        const array = items.filter(item => item)
+        const element = this.shadowRoot.querySelector('.btm')
+        let currentTurn = 0
+        clearInterval(this.setTextInterval)
+        this.setTextInterval = setInterval(() => {
+            // nice and smooth fade transition
+            element.classList.add('fade')
+            element.addEventListener('transitionend', function textUpdate() {
+                element.removeEventListener('transitionend', textUpdate)
+                element.classList.remove('fade')
+
+                element.textContent = array[currentTurn]
+
+                currentTurn == array.length - 1 ? currentTurn = 0 : currentTurn++
+            });
+        }, 7000)
+    }
+
+    keyPressHandler(e) {
+        if (e.key == 'Enter' || e.key == ' ' || e.button == 0) {
+            this.open()
+        }
+    }
+
+    open() {
+        let url = `https://www.yr.no/en/`
+        const coords = this.load?.coords
+        if (coords) url = `https://www.yr.no/en/forecast/graph/${coords.lat},${coords.lon}`
+
+        window.open(url, '_blank')
+        this.shadowRoot.host.blur()
     }
 }
 
